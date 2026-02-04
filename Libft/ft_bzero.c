eb777953e9920ffcae6048cd54928492f9423e0c
Rosa Vela <rosavela@macbook-pro.local>
2026-02-04 01:26:42 +0100

    Funciones mem estudiadas y con su main individual

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -25,3 +25,30 @@ void	ft_bzero(void *str, size_t n)
 		i++;
 	}
 }
+/*
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	char str1[20] = "Alessia Xiuling";
+	char str2[20] = "Alessia Xiuling";
+	size_t n = 5;
+
+	ft_bzero(str1, n);
+	bzero(str2, n);
+	printf("ft_bzero: [%s]\n", str1);
+    printf("bzero : [%s]\n", str2);
+
+	printf("Resto: [%s]\n", str1 + n);
+	printf("Resto: [%s]\n", str2 + n);
+
+	int nums[5] = {1, 2, 3, 4, 5};
+	ft_bzero(nums, sizeof(int) * 2);
+	printf("nums[0]: %d, nums[1]: %d, nums[2]: %d, nums[3]: %d, nums[4]: %d\n", nums[0], nums[1], nums[2], nums[3], nums[4]);
+
+	char word[5] = "Hola";
+	ft_bzero(word, 0);
+	printf("bzero : [%s]\n", word);
+
+	return (0);
+}*/
--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -15,10 +15,13 @@ void	*ft_memcpy(void *dest, const void *str, size_t n)
 {
 	size_t			i;
 	unsigned char	*d;
-	unsigned char	*s;
+	const unsigned char	*s;
+	
+	if(!dest && !str)
+		return (NULL);
 
 	d = (unsigned char *)dest;
-	s = (unsigned char *)str;
+	s = (const unsigned char *)str;
 	i = 0;
 	while (i < n)
 	{
@@ -27,3 +30,28 @@ void	*ft_memcpy(void *dest, const void *str, size_t n)
 	}
 	return (dest);
 }
+/*
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	char src[] = "Alessia Xiuling";
+	char dest[20];
+	char src1[] = "Alessia Xiuling";
+	char dest1[20];
+	printf("ft_memcpy: [%s]\n", (char *)ft_memcpy(dest, src, 16));
+	printf("memcpy: [%s]\n", (char *)ft_memcpy(dest, src, 16));
+
+	int nums[3] =  {100, 200, 300};
+	int dest_nums[3];
+	int nums1[3] =  {100, 200, 300};
+	int dest_nums1[3];
+	ft_memcpy(dest_nums, nums, sizeof(int) * 3);
+	memcpy(dest_nums1, nums1, sizeof(int) * 3);
+	printf("nums: %d, %d, %d\n", dest_nums[0], dest_nums[1], dest_nums[2]);
+	printf("nums1: %d, %d, %d\n", dest_nums1[0], dest_nums1[1], dest_nums1[2]);
+
+	printf("[%s]\n", (char *)ft_memcpy(NULL, NULL, 5));
+	printf("[%s]\n", (char *)memcpy(NULL, NULL, 5));
+	return (0);
+}*/
